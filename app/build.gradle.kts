aed1906b29b1cd0ce5995a0098d5ed15e332048b
AbunaRichard <abunarichard7@gmail.com>
2026-06-24 12:23:48 +0300

    Bump version to 2.0 (Version 2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         minSdk = 24
         //noinspection OldTargetApi
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
